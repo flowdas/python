d1cdd7c611ccab184e6c98b980346f407d7c747c
flowdas <flowdas@gmail.com>
2020-07-04 08:43:19 +0900

    3.9.0b4

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,8 +1,9 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.9.0b3',
+    VERSION: '3.9.0b4',
     LANGUAGE: 'ko',
     COLLAPSE_INDEX: false,
+    BUILDER: 'html',
     FILE_SUFFIX: '.html',
     HAS_SOURCE: false,
     SOURCELINK_SUFFIX: '.txt',
