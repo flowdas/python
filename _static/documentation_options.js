85e9627f165d805c552d5c1683f95b87a4a7f0a9
flowdas <flowdas@gmail.com>
2020-10-06 09:34:41 +0900

    wip

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.9.0rc2',
+    VERSION: '3.9.0',
     LANGUAGE: 'ko',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
